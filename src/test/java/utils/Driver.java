56f32f4e19fea4e9936769111a3be85d418b6924
Nevzat <nevzatcelik151907@gmail.com>
2024-06-24 14:01:19 +0300

    third lesson

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -38,8 +38,8 @@ public class Driver {
             caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, ANDROIDVERSION);
             caps.setCapability(MobileCapabilityType.PLATFORM_NAME, PLATFORM);
             caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, OTOMASYON_ISMI);
-            caps.setCapability("appPackage",ConfigReader.getProperty("kiwiAppPackage"));
-            caps.setCapability("appActivity",ConfigReader.getProperty("kiwiAppActivity"));
+            caps.setCapability("appPackage",ConfigReader.getProperty("allCurrencyPackage"));
+            caps.setCapability("appActivity",ConfigReader.getProperty("allCurrencyActivity"));
             caps.setCapability(MobileCapabilityType.NO_RESET,false);
             /* eger bu capability FALSE olarak kullanilirsa,uygulama test edildikten sonra her seferinde kullanici datalari temizlenir ve
             uygulamanin ilk install haline dondurulur
